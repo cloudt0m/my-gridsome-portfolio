cbc0a49f5653c4ea1fb121a0cfa9ac2ce0bb55d9
homeway <cloudtom@gmail.com>
2020-12-03 12:22:41 +0800

    UPDT add 404 page

---
+++ src/pages/404.vue
@@ -0,0 +1,8 @@
+<template>
+  <Layout>
+    <div class="not-found">
+      <g-image src="../../static/img/not-found.jpg"></g-image>
+      <g-link to="/" class="btn btn--primary">Back to Home</g-link>
+    </div>
+  </Layout>
+</template>
