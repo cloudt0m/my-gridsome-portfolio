0275cba42257afbf60e50d5b78fdc6a2a1dfe93b
tom hsieh <cloudtom@gmail.com>
2021-02-01 13:11:46 +0800

    FIX swiper init before window load event - wrong width

--- src/components/IndexSlider.vue
+++ src/components/IndexSlider.vue
@@ -1,24 +1,27 @@
 <template>
   <div class="works__slider col-12">
-    <swiper
-      ref="mySwiper"
-      :options="swiperOptions"
-    >
-      <swiper-slide class="work-item" v-for="work in works" :key="work.node.id">
-        <img
-          :src="work.node.imgSrc.src"
-          :srcset="work.node.imgSrc.srcset"
-          alt=""
-          class="work-item__cover"
-        />
-        <g-link
-          :to="work.node.path"
-          class="work-item__link"
+    <div v-swiper:mySwiper="swiperOptions">
+      <div class="swiper-wrapper">
+        <div
+          class="work-item swiper-slide"
+          v-for="work in works"
+          :key="work.node.id"
         >
-          <h2 class="work-item__title">{{ work.node.title }}</h2>
-        </g-link>
-      </swiper-slide>
-    </swiper>
+          <img
+            :src="work.node.imgSrc.src"
+            :srcset="work.node.imgSrc.srcset"
+            alt=""
+            class="work-item__cover"
+          />
+          <g-link
+            :to="work.node.path"
+            class="work-item__link"
+          >
+            <h2 class="work-item__title">{{ work.node.title }}</h2>
+          </g-link>
+        </div>
+      </div>
+    </div>
     <button class="swiper-button swiper-button-prev">
       <g-image
         class="icon"
@@ -37,9 +40,22 @@
 </template>
 
 <script>
+import {
+  Swiper as SwiperClass,
+  Navigation,
+  Pagination,
+} from "swiper/swiper.esm";
+import getAwesomeSwiper from "vue-awesome-swiper/dist/exporter";
+
+SwiperClass.use([Pagination, Navigation]);
+const { directive } = getAwesomeSwiper(SwiperClass);
+
 export default {
-  props: ['works'],
+  props: ["works"],
   name: "slider",
+  directives: {
+    swiper: directive,
+  },
   data() {
     return {
       swiperOptions: {
@@ -56,8 +72,13 @@ export default {
   },
   computed: {
     swiper() {
-      return this.$refs.mySwiper.$swiper;
+      return this.mySwiper.$swiper;
     },
+  },
+  mounted() {
+    window.addEventListener("load", () => {
+      this.mySwiper.update();
+    });
   },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -2,12 +2,6 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import Layout from '~/layouts/Default.vue';
-import {
-  Swiper as SwiperClass,
-  Navigation,
-  Pagination,
-} from 'swiper/swiper.esm';
-import getAwesomeSwiper from 'vue-awesome-swiper/dist/exporter';
 import VueScrollTo from 'vue-scrollto';
 import VueSilentBox from 'vue-silentbox';
 import VueFuse from 'vue-fuse';
@@ -37,9 +31,7 @@ export default function(Vue, { appOptions, router, head, isClient }) {
     next();
   });
   Vue.component('Layout', Layout);
-  SwiperClass.use([Pagination, Navigation]);
-  Vue.use(getAwesomeSwiper(SwiperClass));
-  const { Swiper, SwiperSlide } = getAwesomeSwiper(SwiperClass);
+
   Vue.use(VueScrollTo, {
     duration: 500,
     easing: 'ease-out',
